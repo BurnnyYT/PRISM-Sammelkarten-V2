55821165c53f9836bb0eb9f5eea3af4b5df7d131
BurnnyYT <kbabyshark@outlook.de>
2026-04-18 16:18:30 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+Config.Version = '0.0.1'
+
+Config.CardProp = 'prop_collectible_card'
+Config.BinderProp = 'prop_collectible_binder'
+Config.BoosterProp = 'prop_collectible_booster'
+
+Config.CardsPerBooster = 5
+Config.CardImageFolder = 'images/'
+
+Config.GithubVersionURL = 'https://raw.githubusercontent.com/USERNAME/REPOSITORY/main/version.txt'

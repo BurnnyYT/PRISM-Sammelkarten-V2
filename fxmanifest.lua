a70f302506d9d558d6215744ed52f55dd516ebcc
BurnnyYT <kbabyshark@outlook.de>
2026-04-18 16:17:35 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,31 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+author 'BurnnyYT'
+description 'PRISM Sammelkarten System'
+version '0.0.1'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server.lua'
+}
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js',
+    'images/*.png'
+}
+
+ui_page 'html/index.html'

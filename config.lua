81a64766dff0eb3dceaf454cf182bccb0e6a2acf
BurnnyYT <kbabyshark@outlook.de>
2026-04-18 16:20:26 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -9,4 +9,4 @@ Config.BoosterProp = 'prop_collectible_booster'
 Config.CardsPerBooster = 5
 Config.CardImageFolder = 'images/'
 
-Config.GithubVersionURL = 'https://raw.githubusercontent.com/USERNAME/REPOSITORY/main/version.txt'
+Config.GithubVersionURL = 'https://github.com/BurnnyYT/PRISM-Sammelkarten-V2/blob/main/version.txt'
